dcfbfc756c3b8747ed21a0fb88f50d73d78c3be4
lokesh <codelearning2846@gmail.com>
2026-05-29 12:20:48 +0530

    Fix function name from counChr to countChr

--- new.js
+++ new.js
@@ -1,9 +1,10 @@
+//count the characters no.of time occurred
 let obj={}
-function counChr(str){
+function countChr(str){
     for (const ele of str) {
         (obj[ele])? obj[ele]++ :obj[ele]=1;
     }
     return obj
 }
 let str ="hi,am loke, nice to meet you"
-console.log(counChr(str));
+console.log(countChr(str));
