7f3b8f783df6a804b432c65674d32df6daf71e44
loke-netizen <codelearning2846@gmail.com>
2026-06-25 11:27:34 +0530

    set.size comment added

--- missingnumber.js
+++ missingnumber.js
@@ -8,7 +8,7 @@
 // console.log(missingNo([1,2,3,4,5,7,8]));
 function missingNo(arr){
     let set=new Set(arr)
-    let no=arr.length+1
+    let no=arr.length+1//or set.size+1
     for(let i=1;i<=no;i++){
         if(!set.has(i)) return i
     }
