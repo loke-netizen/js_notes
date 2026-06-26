8bae8682547c3cbbc4d561c730c3428cb11934ad
loke-netizen <codelearning2846@gmail.com>
2026-06-26 08:59:34 +0530

    created a Roman to Integer program

--- romanToint.js
+++ romanToint.js
@@ -1,38 +1,26 @@
-function romanToInt(num) {
-    const romanMap = [
-        { value: 1000, symbol: "M" },
-        { value: 900, symbol: "CM" },
-        { value: 500, symbol: "D" },
-        { value: 400, symbol: "CD" },
-        { value: 100, symbol: "C" },
-        { value: 90, symbol: "XC" },
-        { value: 50, symbol: "L" },
-        { value: 40, symbol: "XL" },
-        { value: 10, symbol: "X" },
-        { value: 9, symbol: "IX" },
-        { value: 5, symbol: "V" },
-        { value: 4, symbol: "IV" },
-        { value: 1, symbol: "I" }
-    ];
+function romanToInt(roman){
+    const romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+};
+let total=0
+for(let i=0;i<roman.length;i++){
+    let current=romanMap[roman[i]]
+    let next=romanMap[roman[i+1]]
 
-    let roman = ""
-    for (i = 0; i < romanMap.length; i++) {
-        if (romanMap[i].value == 1) {
-            console.log("iiioionhk   " + i);
-
-        }
-        const { value, symbol } = romanMap[i]
-
-        while (num >= value) {
-            roman += symbol;
-            num -= value
-            if (num === 0) return roman//option
-        }
-        // for (; int >= value; int -= value) {
-        //     roman += symbol
-        // }
-
+    if(next&&current<next){
+        total-=current
+    }
+    else{
+        total+=current
     }
 
 }
-console.log(romanToInt(2000));
+return total
+}
+console.log(romanToInt("MV"));
