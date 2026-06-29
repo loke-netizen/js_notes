945118031230dfc35ce55b58bfbca6de19a3bf51
loke-netizen <codelearning2846@gmail.com>
2026-06-29 18:11:33 +0530

    checks condition with Object.hasOwn()

--- deepClone.js
+++ deepClone.js
@@ -1,30 +1,32 @@
-function deepClone(obj){
-    if(obj===null || typeof obj!=="object") return obj
+function deepClone(obj) {
+    if (obj === null || typeof obj !== "object") return obj
 
-    if(Array.isArray(obj)){
-        let copyArr=[]
+    if (Array.isArray(obj)) {
+        let copyArr = []
         for (let i = 0; i < obj.length; i++) {
-           copyArr[i]=deepClone(obj[i])
+            copyArr[i] = deepClone(obj[i])
         }
         return copyArr
     }
-    let copyObj={}
+    let copyObj = {}
     for (const key in obj) {
-       copyObj[key] =deepClone(obj[key])  
+        if (Object.hasOwn(obj, key)) {// because some times it have inherited with another object so checking is important 
+            copyObj[key] = deepClone(obj[key])
+        }
     }
     return copyObj
 }
-const obj={
+const obj = {
     name: "Loke",
     age: 21,
     address: {
         city: "Thanjavur",
         pin: 613001
     },
-    vehicles:["bike"]
+    vehicles: ["bike"]
 };
-const clone=deepClone(obj)
-obj.address.pin=613006
+const clone = deepClone(obj)
+obj.address.pin = 613006
 clone.vehicles.push("cycle")
-console.log(clone);
 console.log(obj);
+console.log(clone);
