2888e7987d49960d06cf9d5c6df27d93ee64b583
loke-netizen <codelearning2846@gmail.com>
2026-06-29 17:39:21 +0530

    created a deepClone program

---
+++ deepClone.js
@@ -0,0 +1,30 @@
+function deepClone(obj){
+    if(obj===null || typeof obj!=="object") return obj
+
+    if(Array.isArray(obj)){
+        let copyArr=[]
+        for (let i = 0; i < obj.length; i++) {
+           copyArr[i]=deepClone(obj[i])
+        }
+        return copyArr
+    }
+    let copyObj={}
+    for (const key in obj) {
+       copyObj[key] =deepClone(obj[key])  
+    }
+    return copyObj
+}
+const obj={
+    name: "Loke",
+    age: 21,
+    address: {
+        city: "Thanjavur",
+        pin: 613001
+    },
+    vehicles:["bike"]
+};
+const clone=deepClone(obj)
+obj.address.pin=613006
+clone.vehicles.push("cycle")
+console.log(clone);
+console.log(obj);
